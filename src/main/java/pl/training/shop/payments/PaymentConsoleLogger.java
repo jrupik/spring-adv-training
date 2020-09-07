6b056f472bebf27f31601da8fd22219902d85c12
landrzejewski <landrzejewski.poczta@gmail.com>
2020-09-07 11:13:06 +0200

    fix

--- src/main/java/pl/training/shop/payments/PaymentConsoleLogger.java
+++ src/main/java/pl/training/shop/payments/PaymentConsoleLogger.java
@@ -1,16 +1,12 @@
 package pl.training.shop.payments;
 
 import lombok.extern.java.Log;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
 
-@Aspect
 @Log
 public class PaymentConsoleLogger {
 
     private static final String LOG_FORMAT = "A new payment of %s has been initiated";
 
-    @AfterReturning(value = "@annotation(LogPayments)", returning = "payment")
     public void log(Payment payment) {
         log.info(createLogEntry(payment));
     }
