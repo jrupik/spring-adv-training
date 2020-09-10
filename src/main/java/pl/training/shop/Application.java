d0d28a03bceaea496cf932fcca3bc44ae3c95ecc
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-09-10 15:49:25 +0200

    kafka

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -1,28 +1,10 @@
 package pl.training.shop;
 
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.training.shop.producer.KafkaProducer;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
-
-    @Setter
-    @Autowired
-    private KafkaProducer kafkaProducer;
-    @Setter
-    @Value("${training.kafka.mainTopic}")
-    private String mainTopic;
-
-    @PostConstruct
-    public void init() {
-        kafkaProducer.send("Hello Kafka", mainTopic);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
